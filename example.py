2aff1750a684275eeb1c77f11ba1045b33e22272
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2021-12-23 13:27:34 +0000

    FUNC: Create daterange for sleep

--- example.py
+++ example.py
@@ -1,11 +1,15 @@
 from astronomical import sun
 from astronomical.location import Location
+from astronomical.sleep import Requirements
 
 print("""TheOrganist24: Astronomical
 Library of utilities related to astronomical movements
 """)
 
 home = Location("Ivybridge", -3.941355, 50.392189)
+print(home)
+
+requirements = Requirements()
 
 sunrise, sunset = sun.sun_times(home)
 
