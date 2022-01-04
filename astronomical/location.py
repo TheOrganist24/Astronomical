356da3a2fce8bad68f809330479f4b5845937557
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2022-01-04 12:19:26 +0000

    LINT: Type location module

--- astronomical/location.py
+++ astronomical/location.py
@@ -4,17 +4,20 @@
 class Location:
     """Location on the globe."""
 
-    def __init__(self, name, longitude, latitude):
+    def __init__(self,
+                 name: str,
+                 longitude: float,
+                 latitude: float) -> None:
         """Define all instance attributes here."""
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
         self.location = (longitude, latitude)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Procuce code to initialise this class."""
         return f"Location({self.name!r}, {self.longitude}, {self.latitude})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Generate summary of class with minimal inputs."""
         return f"Location \"{self.name}\": ({self.longitude}, {self.latitude})"
