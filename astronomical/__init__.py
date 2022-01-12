db85bbee0cd259754c67d1928bfab0f1414e1c8c
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2022-01-12 10:51:54 +0000

    FUNC: Add sunrise/sunset times to CLI tool

--- astronomical/__init__.py
+++ astronomical/__init__.py
@@ -4,4 +4,11 @@ This module provides information about the sun, moon, seasons and tides
 according to the user's current whereabouts and whenabouts.
 """
 
+from .interfaces.sun import sun_times
+from .interfaces.location import Location
+
+
+home = Location("Ivybridge", -3.9413, 50.3921)
+
 __version__ = "0.1.0"
+sunrise, sunset = sun_times(home)
