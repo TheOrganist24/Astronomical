5397fd9a157af01c6bdb662b4252608eec99de91
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2022-01-17 16:23:31 +0000

    TESTS: Write some basic tests for models

---
+++ tests/model/test_celestials.py
@@ -0,0 +1,21 @@
+# Data classes only hold data so only test that data exist and that incorrect data causes errors
+# this is overkill - remove in future as only example
+
+import pytest
+from astronomical.model import celestials
+
+class TestBodyClass:
+    def test_when_body_is_instatiated_that_values_are_returned(self):
+        test_name = "Not the Earth"
+        test_mass = 2
+        test_radius = 3
+        
+        body = celestials.Body(test_name, test_mass, test_radius)
+        
+        assert body.name == test_name \
+            and body.mass == test_mass \
+            and body.radius == test_radius
+
+    def test_when_body_is_instatiated_without_args_that_failure(self):
+        with pytest.raises(TypeError):
+            body = celestials.Body()
