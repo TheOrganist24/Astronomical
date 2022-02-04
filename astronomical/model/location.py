48ef1855dc49f81c8dcbec35bb0dcca9717c9e4c
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2022-02-04 08:42:06 +0000

    FUNC: Create Alarms class
    Create new Alarms class in "Real World Calculations" module, but still need
    to fill in "Calculate Wake" method.

--- astronomical/model/location.py
+++ astronomical/model/location.py
@@ -1,6 +1,7 @@
 """This module provides the dataclasses for location in astronomical."""
 
 from dataclasses import dataclass
+from datetime import time, timedelta
 
 from ..service.logging import logger
 
@@ -12,6 +13,20 @@ class Location:
     name: str
     longitude: float
     latitude: float
+
+    def __post_init__(self):
+        """Add DEBUG logging to classes."""
+        logger.debug(f"CLASS: \"{self.__class__.__name__}\" instantiated.")
+
+
+@dataclass
+class Requirements:
+    """Sleep requirements."""
+
+    sleep: timedelta = timedelta(hours=8)
+    latest_wake_up: time = time(hour=7)
+    earliest_wake_up: time = time(hour=6)
+    ablutions: timedelta = timedelta(hours=1)
 
     def __post_init__(self):
         """Add DEBUG logging to classes."""
--- astronomical/model/real_world_calculations.py
+++ astronomical/model/real_world_calculations.py
@@ -1,1 +1,24 @@
 """This module calculates sunrise etc."""
+
+from datetime import datetime, timedelta
+
+from astronomical.model.location import Requirements
+from astronomical.model.solar_system import PlanetaryLocation
+
+
+class Alarms:
+    """Alarms object."""
+
+    def __init__(self, requirements: Requirements,
+                 location: PlanetaryLocation) -> None:
+        """Initialise variables."""
+        self.needs: Requirements = requirements
+        self.locale: PlanetaryLocation = location
+        self.wake: datetime = self._calculate_wake()
+        self.sleep: datetime = self.wake + self.needs.sleep
+        self.work: datetime = self.wake + self.needs.ablutions
+
+    def _calculate_wake(self) -> datetime:
+        """Calculate wake-up time."""
+        # Add code here to calculate wake-up time
+        return datetime.now()
