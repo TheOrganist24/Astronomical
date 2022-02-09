3d1e2c35dc0e33a2c2379ca509afb2b400055286
Nicholas Courtman <nicholas.courtman@ukho.gov.uk>
2022-02-09 10:28:39 +0000

    FUNC: Tidy up Default Service

--- astronomical/service/configuration.py
+++ astronomical/service/configuration.py
@@ -1,6 +1,7 @@
 """Utility module for setting configurations."""
 
 from datetime import datetime
+from typing import Any, Dict
 
 from astronomical.interface.configuration import UserDefaults
 from astronomical.model.configuration import Defaults
@@ -11,29 +12,26 @@ class DefaultService(Defaults):
 
     def __init__(self, user_defaults: UserDefaults, instant: datetime) -> None:
         """Initialise variables."""
+        default_data: Dict[str, Any] \
+            = self._create_kwargs_for_super(user_defaults, instant)
+        super().__init__(**default_data)
+
+    def _create_kwargs_for_super(self, user_defaults, instant):
+        """Create keyword arguments dictionary for initialising parent.
+
+        A dictionary of kwargs will allow for those unset to fall back to the
+        default values as set in the parent; in this case, the Model's
+        Defaults class.
+        """
+        default_data: Dict[str, Any] = {}
         if user_defaults.loaded:
-            location = user_defaults.location
-            longitude = user_defaults.longitude
-            latitude = user_defaults.latitude
+            default_data["instant"] = instant
+            default_data["location"] = user_defaults.location
+            default_data["longitude"] = user_defaults.longitude
+            default_data["latitude"] = user_defaults.latitude
+
             if user_defaults.locale is not None:
-                planet = user_defaults.locale.planet
-                if user_defaults.sleep is not None:
-                    sleep = user_defaults.sleep
-                    super().__init__(location=location, longitude=longitude,
-                                     latitude=latitude,
-                                     planet=planet,  # type: ignore
-                                     instant=instant, sleep=sleep)
-                else:
-                    super().__init__(location=location, longitude=longitude,
-                                     latitude=latitude,
-                                     planet=planet,  # type: ignore
-                                     instant=instant)
-            else:
-                if user_defaults.sleep is not None:
-                    sleep = user_defaults.sleep
-                    super().__init__(location=location, longitude=longitude,
-                                     latitude=latitude, instant=instant,
-                                     sleep=sleep)
-                else:
-                    super().__init__(location=location, longitude=longitude,
-                                     latitude=latitude, instant=instant)
+                default_data["planet"] = user_defaults.locale.planet
+            if user_defaults.sleep is not None:
+                default_data["sleep"] = user_defaults.sleep
+        return default_data
